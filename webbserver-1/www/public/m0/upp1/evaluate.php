f322fba45895f1d6f17da823e74eee341e4f42a8
Ferit Bat <feritbat6@gmail.com>
2025-08-19 12:02:56 +0200

    1

--- webbserver-1/www/public/m0/upp1/evaluate.php
+++ webbserver-1/www/public/m0/upp1/evaluate.php
@@ -1,30 +1,33 @@
 <!doctype html>
 <html>
+
 <head lang="sv"></head>
+
 <body>
-<h1>Matematiktest</h1>
-<h2>Resultat</h2>
-<?php
-     	$ans1 = $_POST['q1'];
-     	$ans2 = $_POST['q2'];
-        $ans3 = $_POST['q3'];
-        $ans4 = $_POST['q4'];
-        $ans5 = $_POST['q5'];
+   <h1>Matematiktest</h1>
+   <h2>Resultat</h2>
+   <?php
+   $ans1 = $_POST['q1'];
+   $ans2 = $_POST['q2'];
+   $ans3 = $_POST['q3'];
+   $ans4 = $_POST['q4'];
+   $ans5 = $_POST['q5'];
 
-     	$points = 0;
-     	
-     	if($ans1 == 9)
-           $points++;
-     	if($ans2 == 15)
-           $points++;
-        if($ans3 == 15)
-           $points++;    
-        if($ans4 == 15)
-           $points++;
-        if($ans5 == 15)
-           $points++;
+   $points = 0;
 
-     	echo("<p>Du fick " . $points . " av 5 möjliga</p>");
-?>
+   if ($ans1 == 9)
+      $points++;
+   if ($ans2 == 15)
+      $points++;
+   if ($ans3 == 15)
+      $points++;
+   if ($ans4 == 15)
+      $points++;
+   if ($ans5 == 15)
+      $points++;
+
+   echo ("<p>Du fick " . $points . " av 5 möjliga</p>");
+   ?>
 </body>
+
 </html>
