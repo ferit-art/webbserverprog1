587355e952a194a0be9b4c7bfe876aff96595973
Ferit Bat <feritbat6@gmail.com>
2025-09-14 20:37:37 +0200

    update m2

--- webbserver-1/www/public/m2/upp9/Person.php
+++ webbserver-1/www/public/m2/upp9/Person.php
@@ -16,9 +16,3 @@ class Person
         $this->pwd = $pwd;
     }
 }
-
-// $userArray = [];
-//
-//$userArray[] = new Person("Gabriella", "18", "12 12 12", "gabbe", "12345");
-//$userArray[] = new Person("Ozzy", "70", "12 12", "ozi", "123444");
-//$userArray[] = new Person("Gella", "20", "20 12 12", "gela", "3333");
